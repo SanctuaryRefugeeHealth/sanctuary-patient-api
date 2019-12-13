4cb925cc300e8d0b648b91437e54cb36c4421b7d
Madeleine Currie <madeleinecurrie@Madeleines-MacBook-Pro-2.local>
2019-12-13 15:13:40 -0500

    unbreak get appointments

--- src/api/handlers/appointments/getAppointments.js
+++ src/api/handlers/appointments/getAppointments.js
@@ -1,7 +1,10 @@
 const { db } = require("../../../db");
 
 export default (req, res) => {
+    /*
     const { phoneNumber } = req.query;
+
+    console.log(phoneNumber);
 
     const appointments = patientPhoneNumber
         ? db("appointments")
@@ -12,5 +15,38 @@ export default (req, res) => {
               .select("*")
               .execute();
 
+    res.status(200).send(appointments);
+
+    */
+
+    // TODO: Fetch appointments
+    const appointments = [
+        {
+            appointmentId: 1,
+            patientName: "Michael Scott",
+            patientPhoneNumber: "1-226-555-1234",
+            patientLanguage: "english",
+            practitionerName: "Dr. Meridith Grey",
+            practitionerClinicName: "Grand River Hospital",
+            practitionerAddress: "835 King St W, Kitchener",
+            practitionerPhoneNumber: "1-226-555-9999",
+            appointmentTime: "2020-01-01 12:30:00",
+            appointmentIsConfirmed: true
+        },
+        {
+            appointmentId: 1,
+            patientName: "Dwtight Schrute",
+            patientPhoneNumber: "1-226-555-6660",
+            patientLanguage: "english",
+            practitionerName: "Dr. Cristina Yang",
+            practitionerClinicName: "Grand River Hospital",
+            practitionerAddress: "835 King St W, Kitchener",
+            practitionerPhoneNumber: "1-226-555-0000",
+            appointmentTime: "2020-02-14 11:30:00",
+            appointmentIsConfirmed: false
+        }
+    ];
+
+    // Demo data
     res.status(200).send(appointments);
 };
