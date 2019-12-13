d7d62bd7448495da52ca22b77ea0d70bd16330ce
Madeleine Currie <madeleinecurrie@Madeleines-MacBook-Pro-2.local>
2019-12-13 14:59:24 -0500

    get appointments uses db

--- src/api/handlers/appointments/getAppointments.js
+++ src/api/handlers/appointments/getAppointments.js
@@ -1,32 +1,16 @@
-export default (req, res) => {
-    // TODO: Fetch appointments
-    const appointments = [
-        {
-            appointmentId: 1,
-            patientName: "Michael Scott",
-            patientPhoneNumber: "1-226-555-1234",
-            patientLanguage: "english",
-            practitionerName: "Dr. Meridith Grey",
-            practitionerClinicName: "Grand River Hospital",
-            practitionerAddress: "835 King St W, Kitchener",
-            practitionerPhoneNumber: "1-226-555-9999",
-            appointmentTime: "2020-01-01 12:30:00",
-            appointmentIsConfirmed: true
-        },
-        {
-            appointmentId: 1,
-            patientName: "Dwtight Schrute",
-            patientPhoneNumber: "1-226-555-6660",
-            patientLanguage: "english",
-            practitionerName: "Dr. Cristina Yang",
-            practitionerClinicName: "Grand River Hospital",
-            practitionerAddress: "835 King St W, Kitchener",
-            practitionerPhoneNumber: "1-226-555-0000",
-            appointmentTime: "2020-02-14 11:30:00",
-            appointmentIsConfirmed: false
-        }
-    ];
+const { db } = require("../../../db");
 
-    // Demo data
+export default (req, res) => {
+    const { phoneNumber } = req.query;
+
+    const appointments = patientPhoneNumber
+        ? db("appointments")
+              .select("*")
+              .where("patientPhoneNumber", phoneNumber)
+              .execute()
+        : db("appointments")
+              .select("*")
+              .execute();
+
     res.status(200).send(appointments);
 };
