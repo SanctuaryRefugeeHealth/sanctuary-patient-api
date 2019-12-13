e4bc5b44c9c0b1daf84b8b67ed1538ae12081eca
Ankit Patel <apatel@gobonfire.com>
2019-12-13 11:46:04 -0500

    Init templates route w/ mocked model

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,16 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
-import facets from './facets';
+import { version } from "../../package.json";
+import { Router } from "express";
+import templates from "./templates";
 
 export default ({ config, db }) => {
-	let api = Router();
+  let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+  api.use("/templates", templates({ config, db }));
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+  // perhaps expose some API metadata at the root
+  api.get("/", (req, res) => {
+    res.json({ version });
+  });
 
-	return api;
-}
+  return api;
+};
