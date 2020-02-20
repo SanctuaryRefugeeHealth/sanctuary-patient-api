16cd5e7b8236d38f05a4d590746d855aa2ff5b62
Marlin Gingerich <mgingerich@gobonfire.com>
2020-02-20 15:58:05 -0500

    first pass docker compose

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 module.exports = {
     client: "mysql",
     connection: {
+        host: "db",
+        port: 3306,
         user: "root",
         password: "",
         database: "sanctuary_dev"
