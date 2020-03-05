46dc03c85afafe87c00baffdebed3c1ed4a31144
Mohammed J. Hakmi <mhakmi@gobonfire.com>
2020-03-05 15:19:47 -0500

    install dotenv

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import 'dotenv/config';
 import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
