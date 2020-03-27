9029f277529152451296b6c53aa50d619deb636f
clark <clark@focus21.io>
2020-03-27 16:52:44 -0400

    get message by appt ID

--- src/api/handlers/messages/getMessages.js
+++ src/api/handlers/messages/getMessages.js
@@ -1,26 +1,15 @@
+const { db } = require("../../../../knex");
+
 export default (req, res) => {
-    const { appointmentId } = req.params;
+  const { appointmentId } = req.params;
 
-    // TODO: Fetch messages and replies for appointmentId
-    const messages = [
-        {
-            messageId: 1,
-            appointmentId: 1,
-            templateName: "Appointment Reminder",
-            messageBody: "Hello World",
-            timeSent: "2019-12-13 14:10:00",
-            language: "english"
-        },
-        {
-            messageId: 1,
-            appointmentId: 1,
-            templateName: "Additional Information",
-            messageBody: "Please call (226) 123-4567 for more details",
-            timeSent: "2019-12-13 14:10:00",
-            language: "arabic"
-        }
-    ];
-
-    // Demo data
-    res.status(200).send(messages);
+  db("messages")
+    .select("*")
+    .where("appointmentId", appointmentId)
+    .then(result => {
+      res.status(200).send(result);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 };
