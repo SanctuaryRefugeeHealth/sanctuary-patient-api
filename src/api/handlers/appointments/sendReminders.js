d9d01ff833d0c4908458b10caf3975a5d546e4ce
clark <clark@focus21.io>
2020-04-01 18:45:40 -0400

    batch replies now saves successful messages to database

--- src/api/handlers/appointments/sendReminders.js
+++ src/api/handlers/appointments/sendReminders.js
@@ -24,17 +24,47 @@ export default async (req, res) => {
   }
 
   const appointmentsPromises = appointments.map(appointment => {
-    const language = LanguagesModel.getByLanguageString(appointment.patientLanguage);
-    // 1 is reminder template ID
+    const {
+      appointmentId,
+      patientLanguage,
+      patientPhoneNumber
+    } = appointment;
+  
+    const language = LanguagesModel.getByLanguageString(patientLanguage);
+    // 1 is reminder template ID, full templated needed for name in message record
+    const template = TemplatesModel.getById(1);
     const messageBody = TemplatesModel.generateMessage(1, language.id, appointment);
 
+    const message = {
+      appointmentId,
+      messageBody,
+      language: language.id,
+      templateName: template.templateName,
+      // UTC, will be the same for every message sent in a batch
+      timeSent: moment().format("YYYY-MM-DD HH:mm:ss")
+    };
+
     return new Promise((resolve) => {
-      sendMessage(appointment.patientPhoneNumber, messageBody)
-        .then(() => resolve(appointment))
+      sendMessage(patientPhoneNumber, messageBody)
+        .then(async () => {
+          let messageId;
+          try {
+            messageId = await db("messages").insert(message);
+          } catch (error) {
+            res.status(500).send({
+              error,
+              message: "Could not save message to database"
+            });
+          }
+          return resolve({
+            appointmentId,
+            messageId: messageId[0]
+          });
+        })
         // TODO: don't resolve this, create a log or send a notification to someone
         .catch((error) => resolve({
             error,
-            appointmentId: appointment.id
+            appointmentId
           })
         );
     });
