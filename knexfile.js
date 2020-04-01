bb48e98abdedbb8c61f0586cd163d4e823f4f434
Steve Davis <hmsdavis@gmail.com>
2020-04-01 17:14:50 -0400

    Switch to bcryptjs

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
     client: "mysql",
     connection: {
-        host: process.env.MYSQL_HOSTNAME,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DB
+        host: process.env.MYSQL_HOSTNAME || "localhost",
+        port: process.env.MYSQL_PORT || "3306",
+        user: process.env.MYSQL_USERNAME || "root",
+        password: process.env.MYSQL_PASSWORD || "password",
+        database: process.env.MYSQL_DB || "sanctuary_dev"
     }
 };
--- src/api/handlers/auth/getToken.js
+++ src/api/handlers/auth/getToken.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { db } from "../../../../knex";
 import { jwtConfig } from "../../../config";
