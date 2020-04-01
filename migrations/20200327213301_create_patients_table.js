c2662a3b1aa982280697f3574f71d4b7f3f1777f
Steve Davis <hmsdavis@gmail.com>
2020-03-31 20:54:23 -0400

    Update Seed Script; Update Migration

--- migrations/20200327213301_create_patients_table.js
+++ migrations/20200327213301_create_patients_table.js
@@ -1,68 +1,12 @@
 
 exports.up = function (knex) {
-  // Create new patient table
-  return knex.schema.createTable("patients", (table) => {
-    table.increments("id").primary();
-    table.text("name");
-    table.text("phoneNumber");
-    table.enum("language", ["english","arabic","spanish"]);
-  })
-    // Get all patients and add foreign key to appointments table
-    .then(() => {
-      return Promise.all([
-        knex.select("appointmentId", "patientName", "patientPhoneNumber", "patientLanguage").from("appointments"),
-        knex.schema.table("appointments", (table) => {
-          table.integer("patientId").unsigned();
-          table.foreign("patientId").references("patients.id").onDelete("cascade");
-        })
-      ])
-    })
-    // Insert all patients from appointments into patient table and update the foreign key in appointments
-    .then(([patients]) => 
-      Promise.all(patients.map((patient) => {
-        return knex("patients").insert({name: patient.patientName, phoneNumber: patient.patientPhoneNumber, language: patient.patientLanguage})
-          .then((patientId) => {
-            return knex("appointments")
-              .where("appointmentId", "=", patient.appointmentId)
-              .update({ patientId: patientId});
-          });
-      }))
-    )
-    // Drop the columns that are no longer needed
-    .then(() =>
-      knex.schema.table("appointments", (table) => {
-        table.dropColumns("patientName", "patientPhoneNumber", "patientLanguage");
-      })
-    );
+  return knex.schema.alterTable("appointments", function(table) {
+    table.datetime("appointmentTime").notNullable().alter();
+  });
 };
 
 exports.down = function(knex) {
-  // Get patients; Put the removed columns back into appointments
-  return Promise.all([
-    knex.select("*").from("appointments").innerJoin("patients", "appointments.patientId", "patients.id"),
-    knex.schema.table("appointments", (table) => {
-      table.text("patientName");
-      table.text("patientPhoneNumber");
-      table.enum("patientLanguage", ["english","arabic","spanish"]);
-    })
-  ])
-    // Move the patients back into their appointments
-    .then(([appointments]) =>
-      Promise.all(
-        appointments.map((appointment) => {
-          return knex("appointments")
-            .where("appointmentId", "=", appointment.appointmentId)
-            .update({ patientName: appointment.name, patientPhoneNumber: appointment.phoneNumber, patientLanguage: appointment.language});
-        })
-      )
-    )
-    // Remove the foreign key
-    .then(() => 
-      knex.schema.table("appointments", (table) => {
-        table.dropForeign("patientId");
-        table.dropColumn("patientId");
-      })
-    )
-    // Drop the new table
-    .then(() =>knex.schema.dropTable("patients"));
+  return knex.schema.alterTable("appointments", function(table) {
+    table.text("appointmentTime").notNullable().alter();
+  });
 };
---
+++ seeds/appointments.js
@@ -0,0 +1,33 @@
+
+const random_name = require("node-random-name");
+
+const addDays = (date, days) => {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+};
+
+exports.seed = function(knex) {
+  return knex("appointments").del()
+    .then(() => {
+      const promises = [];
+      const languages = ["english", "arabic", "spanish"]
+      for (let i = 0; i <= 10; i++) {
+        let doctorName = random_name({ seed: `Doctor names ${i}` });
+        promises.push(
+          knex("appointments").insert({
+            patientName: random_name({ seed: `Patient names ${i}` }), 
+            patientPhoneNumber: "555-534-2987", 
+            patientLanguage: languages[Math.floor(Math.random() * languages.length)],
+            practitionerName: `Dr. ${doctorName}`,
+            practitionerClinicName: `${doctorName} Clinic`,
+            practitionerAddress: "123 Fake St.",
+            practitionerPhoneNumber: "555-555-5555",
+            appointmentTime: addDays(new Date(), (Math.floor(Math.random() * Math.floor(28)))),
+            appointmentIsConfirmed: Math.round(Math.random())
+          })
+        )
+      }
+      return Promise.all(promises);
+    });
+};
