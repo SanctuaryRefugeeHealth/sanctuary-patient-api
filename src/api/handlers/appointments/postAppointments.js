e7468065f333e220bf179020b37d0a024c7eec4e
clark <clark@focus21.io>
2020-04-02 09:50:44 -0400

    send reminder message immediately when appointment is created

--- src/api/handlers/appointments/postAppointments.js
+++ src/api/handlers/appointments/postAppointments.js
@@ -1,6 +1,10 @@
+import moment from "moment";
 import { db } from "../../../../knex";
+import TemplatesModel from "../../../models/templates";
+import LanguagesModel from "../../../models/languages";
+import { sendMessage } from "../../../services/twilioClient";
 
-export default (req, res) => {
+export default async (req, res) => {
   const {
     date,
     location,
@@ -24,9 +28,52 @@ export default (req, res) => {
     appointmentIsConfirmed: false
   };
 
-  db("appointments")
-  .insert(appointment)
-  .then(result => {
-    res.status(201).send(result);
-  });
+  let insertedAppointmentId;
+  try {
+    insertedAppointmentId = await db("appointments").insert(appointment);
+  } catch (error) {
+    res.status(500).send({
+      error,
+      message: `Could not insert appointment for ${patientPhoneNumber}`
+    });
+    return;
+  }
+
+  const language = LanguagesModel.getByLanguageString(patientLanguage);
+  const template = TemplatesModel.getById(1);
+  const messageBody = TemplatesModel.generateMessage(1, language.id, appointment);
+
+  const message = {
+    appointmentId: insertedAppointmentId,
+    messageBody,
+    language: language.id,
+    templateName: template.templateName,
+    // UTC
+    timeSent: moment().format("YYYY-MM-DD HH:mm:ss")
+  };
+
+  sendMessage(patientPhoneNumber, messageBody)
+    .then(async () => {
+      let messageId;
+      try {
+        messageId = await db("messages").insert(message);
+      } catch (error) {
+        res.status(500).send({
+          error,
+          message: "Could not save message to database"
+        });
+      }
+      res.status(200).send({
+        insertedAppointmentId,
+        messageId: messageId[0]
+      });
+      return;
+    })
+    .catch((error) => {
+      res.status(500).send({
+        error,
+        insertedAppointmentId
+      })
+      return;
+    });
 };
