b5dbdf3866f28fb0d11875ceafe8b725ae4c238a
Steve Davis <hmsdavis@gmail.com>
2020-04-22 23:43:21 -0400

    Add Translations

--- src/models/templates.js
+++ src/models/templates.js
@@ -4,14 +4,25 @@ const templates = [
   {
     templateId: 1,
     templateName: "Appointment Reminder",
-    1: `Hello,
-    this is a message from Dr. Michael Stephenson's office for {{patientName}}.
-    You have an appointment with Dr. {{practitionerClinicName}} on {{appointmentTime}} at the following address {{practitionerAddress}}.`,
-    2: `مرحباً،
-    هذه الرسالة مرسلة من مكتب الدكتور مايكل ستيفينسون إلى {{patientName}}  .
-    لديك(ي) موعد لدى {{practitionerClinicName}}  .
-    في تاريخ {{appointmentTime}}
-    على العنوان التالي {{practitionerAddress}}`,
+    // English
+    1: `This is a message from Dr. Michael Stephenson's office - Sanctuary Refugee Health Centre to {{patientName}}. 
+    You have an appointment for (Consult/ Imaging) on {{appointmentTime}} at the the following address {{practitionerAddress}}. 
+    If have any questions, please call us at 226-336-1321.`,
+    // Arabic
+    2: `هذه رسالة من عيادة الدكتور مايكل طبيب العائلة إلى ( {{patientName}}). هناك موعد ({{practitionerAddress}}) يوم ({{appointmentTime}}) على العنوان التالي: (العنوان)
+    للاستفسار رجاءًا الاتصال على الرقم التالي:`,
+    // Amharic
+    3: `ይህከዶ / ር ሚካኤል እስጢፋኖስ ቢሮ - ቅዱስ ሥደተኞች የጤና ማእከል ለ {{patientName}} መልእክት ነው ፡፡ በሚተቀለው አድራሻ {{practitionerAddress}} ላይ {{appointmentTime}} ቀጠሮ አልዎት ፡፡
+    ማናቸውምጥያቄዎች ካሉዎት እባክዎን በ 226-336-1321 ይደውሉልን ፡`,
+    // Somali
+    4: `Tani waa dhambaal ka socda xafiiska Dr.Michael Steaphenson ee Sanctuary ({{patientName}}). Ballan ayaad u leedahay (La-tashi / Sawir Raajo or Computer) maalinta {Taariikhda} ({{practitionerAddress}})cinwaanka soo socda.
+    Haddii aad qabtid wax su'aalo ah, fadlan naga soo wac Numbarkan.226-336-1321.`,
+    // Turkish
+    5: `Sayin {{patientName}},  {{appointmentTime}} tarihinde  {{practitionerAddress}} adresinde bulunan (Consult/ Imaging) ile randevunuz vardir. Sorulariniz icin bizi 226-336-1321'den arayabilirsiniz.
+    Dr Michael Stephensoniun - Sanctuary Refugee Health Centre`,
+    // Spanish
+    6: `"Este es un mensaje de la oficina del Dr. Michel Stephenson - Sanctuary Refugee Health Centre para: {{patientName}}. Usted tiene una cita con (Consult)/(Imaging)el {{appointmentTime}}en la siguiente dirección: {{practitionerAddress}}. 
+    Si usted tiene alguna pregunta, por favor llamenos al 226-336-1321."`
   },
   {
     templateId: 2,
