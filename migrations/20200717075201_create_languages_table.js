f5cb89b8704a05bb66aa3e19ca8f8c7d9fe7e13b
Sam Lewis <sam@wiles.co>
2020-07-18 12:03:48 -0400

    Switched Tigrinya to Amheric. Set charset on languages table.

--- migrations/20200717075201_create_languages_table.js
+++ migrations/20200717075201_create_languages_table.js
@@ -1,21 +1,22 @@
 exports.up = (knex) => {
-    return knex.schema
-      .createTable("languages", (table) => {
-        table.string("name").primary();
-      })
-      .then(() => {
-        return knex("languages").insert([
-            {name:"English"},
-            {name:"Arabic"},
-            {name:"Spanish"},
-            {name:"Tigrinya"},
-            {name:"Somali"},
-            {name:"Turkish"},
-        ]);
-      });
-  };
-  
-  exports.down = (knex) => {
-    return knex.schema.dropTableIfExists("languages");
-  };
-  
+  return knex.schema
+    .createTable("languages", (table) => {
+      table.string("name", 255).primary();
+
+      table.charset("utf8");
+    })
+    .then(() => {
+      return knex("languages").insert([
+        { name: "English" },
+        { name: "Arabic" },
+        { name: "Spanish" },
+        { name: "Amharic" },
+        { name: "Somali" },
+        { name: "Turkish" },
+      ]);
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists("languages");
+};
