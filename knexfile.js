d2fefb288083396e7f9a32dff5a71f05fb6648e2
Sam Lewis <sam@wiles.co>
2020-07-22 12:49:18 -0400

    Merge fix

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,12 @@ module.exports = {
         host: process.env.MYSQL_HOSTNAME || "localhost",
         port: process.env.MYSQL_PORT || "3306",
         user: process.env.MYSQL_USERNAME || "root",
+        debug: process.env.NODE_ENV === "development",
         password: process.env.MYSQL_PASSWORD || "password",
-        database: process.env.MYSQL_DB || "sanctuary_dev"
+        database: process.env.MYSQL_DB || "sanctuary_dev",
+        pool: {
+            refreshIdle: false,
+        }
     }
+  
 };
