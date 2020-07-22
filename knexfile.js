8440e3abcb33feb57a6a465c6338aaa7d833c925
Sam Lewis <sam@wiles.co>
2020-07-22 12:51:57 -0400

    Reverted knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,8 @@ module.exports = {
         host: process.env.MYSQL_HOSTNAME || "localhost",
         port: process.env.MYSQL_PORT || "3306",
         user: process.env.MYSQL_USERNAME || "root",
-        debug: process.env.NODE_ENV === "development",
         password: process.env.MYSQL_PASSWORD || "password",
-        database: process.env.MYSQL_DB || "sanctuary_dev",
-        pool: {
-            refreshIdle: false,
-        }
+        database: process.env.MYSQL_DB || "sanctuary_dev"
     }
   
 };
