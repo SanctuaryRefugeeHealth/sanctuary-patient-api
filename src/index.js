cfe91026b7f1c518478acfa20e0fe29285448475
Sam Lewis <sam@wiles.co>
2020-07-27 13:14:16 -0400

    Converted API to use express-jwt

--- src/index.js
+++ src/index.js
@@ -1,43 +1,67 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import 'dotenv/config';
-import express from 'express';
-import http from 'http';
-import morgan from 'morgan';
-import api from './api';
-import config from './config';
-import initializeDb from './db';
-import middleware from './middleware';
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import morgan from "morgan";
+import jwt from "express-jwt";
+import api from "./api";
+import config from "./config";
+import initializeDb from "./db";
 
 let app = express();
 app.server = http.createServer(app);
 
 // logger
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // 3rd party middleware
-app.use(cors({
-	exposedHeaders: config.corsHeaders
-}));
+app.use(
+  cors({
+    exposedHeaders: config.corsHeaders,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({
-	limit : config.bodyLimit
-}));
+app.use(
+  bodyParser.json({
+    limit: config.bodyLimit,
+  })
+);
+
+app.use(
+  jwt({
+    secret: config.jwtConfig.jwtSecret,
+    algorithms: ["HS256"],
+    getToken: (req) => {
+      console.log(req.headers);
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+      ) {
+        return req.headers.authorization.split(" ")[1];
+      }
+
+      return req.headers["x-access-token"];
+    },
+  }).unless({
+    path: [
+      "/api/twilio/reply", // Authentication handled by Twilio middleware
+      "/api/ping",
+      "/api/auth",
+    ],
+  })
+);
 
 // connect to db
-initializeDb(db => {
+initializeDb((db) => {
+  // api router
+  app.use("/api", api({ config, db }));
 
-	// internal middleware
-	app.use(middleware({ config, db }));
-
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(config.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
-	});
+  app.server.listen(config.port, () => {
+    console.log(`Started on port ${app.server.address().port}`);
+  });
 });
 
 export default app;
