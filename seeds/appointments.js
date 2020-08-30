fb4ff8e4f5934c14dda2c884819d2e59848ac112
Sam Lewis <sam@wiles.co>
2020-08-30 10:18:04 -0400

    Fixed seed data for new appointment fields

--- seeds/appointments.js
+++ seeds/appointments.js
@@ -1,7 +1,5 @@
-
 const random_name = require("node-random-name");
 const moment = require("moment");
-
 
 // https://stackoverflow.com/questions/563406/add-days-to-javascript-date
 const addDays = (date, days) => {
@@ -11,31 +9,37 @@ const addDays = (date, days) => {
 };
 
 exports.seed = (knex) => {
-  const languages = ["english", "arabic", "spanish", "amharic", "somali", "turkish"]
+  const languages = [
+    "english",
+    "arabic",
+    "spanish",
+    "amharic",
+    "somali",
+    "turkish",
+  ];
 
-  return Promise.all([
-    knex("replies").del(),
-    knex("messages").del()
-  ])
-    .then(() => { knex("appointments").del() })
+  return Promise.all([knex("replies").del(), knex("messages").del()])
+    .then(() => {
+      knex("appointments").del();
+    })
     .then(() => {
       const promises = [];
 
       for (let i = 0; i <= 10; i++) {
-        let doctorName = random_name({ seed: `Doctor names ${i}` });
         promises.push(
           knex("appointments").insert({
-            patientName: random_name({ seed: `Patient names ${i}` }), 
-            patientPhoneNumber: "555-555-5555", 
-            patientLanguage: languages[Math.floor(Math.random() * languages.length)],
-            practitionerName: `Dr. ${doctorName}`,
-            practitionerClinicName: `${doctorName} Clinic`,
+            patientName: random_name({ seed: `Patient names ${i}` }),
+            patientPhoneNumber: "5555555555",
+            language:
+              languages[Math.floor(Math.random() * languages.length)],
             practitionerAddress: "123 Fake St.",
-            practitionerPhoneNumber: "555-555-5555",
-            appointmentTime: addDays(new Date(), (Math.floor(Math.random() * Math.floor(28)))),
-            appointmentIsConfirmed: Math.round(Math.random())
+            appointmentTime: addDays(
+              new Date(),
+              Math.floor(Math.random() * Math.floor(28))
+            ),
+            appointmentIsConfirmed: Math.round(Math.random()),
           })
-        )
+        );
       }
       return Promise.all(promises);
     })
@@ -48,25 +52,26 @@ exports.seed = (knex) => {
             appointmentId,
             templateName: "Appointment Reminder",
             // Template not important right now for seed.
-            messageBody: "Hello, \
+            messageBody:
+              "Hello, \
               this is a message from Dr. Michael Stephenson's office for {{patientName}}. \
-              You have an appointment with Dr. {{practitionerClinicName}} on {{appointmentTime}} \
-              at the following address {{practitionerAddress}}.", 
+              You have an appointment on {{appointmentTime}} \
+              at the following address {{practitionerAddress}}.",
             timeSent: moment().format("YYYY-MM-DD HH:mm:ss"),
             language: languages[Math.floor(Math.random() * languages.length)],
           })
-        )
+        );
 
         promises.push(
           knex("replies").insert({
             appointmentId,
-            phoneNumber: `555-555-5555`,
-            body: "Thank you for the reminder!", 
-            time: moment().format("YYYY-MM-DD HH:mm:ss")
+            phoneNumber: `5555555555`,
+            body: "Thank you for the reminder!",
+            time: moment().format("YYYY-MM-DD HH:mm:ss"),
           })
-        )
+        );
       });
 
       return Promise.all(promises);
-    })
+    });
 };
