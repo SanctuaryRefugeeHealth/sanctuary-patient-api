b6d3c2a392e2d5172e2f1bd82060eef038510708
Sam Lewis <sam@wiles.co>
2020-10-24 15:45:03 +0100

    Setup cron to send reminder messages and refactored sendReminders to be reusable

--- src/api/handlers/appointments/sendReminders.js
+++ src/api/handlers/appointments/sendReminders.js
@@ -8,75 +8,72 @@ const daysFromNow = (interval) => {
   return moment().add(interval, "d").format("YYYY-MM-DD hh:mm:ss");
 };
 
-export default async (req, res) => {
-  let appointments;
-  try {
-    appointments = await db("appointments")
-      .select("*")
-      .where("isDeleted", false)
-      .where("appointmentTime", ">=", daysFromNow(1))
-      .where("appointmentTime", "<", daysFromNow(3));
-  } catch (error) {
-    res.status(500).send({
-      error,
-      message: `Could not retrieve appointments for ${daysFromNow(
-        1
-      )} and ${daysFromNow(2)}`,
-    });
-    return;
-  }
+export const sendReminder = async (appointment) => {
+  console.log(`Sending reminder for appointment ${appointment.id}`);
 
-  const appointmentsPromises = appointments.map(async (appointment) => {
-    const { appointmentId, patientLanguage, patientPhoneNumber } = appointment;
-
-    const language = await LanguagesModel.getByLanguageString(patientLanguage);
-    // 1 is reminder template ID, full templated needed for name in message record
-    const template = TemplatesModel.getById(1);
-    const messageBody = TemplatesModel.generateMessage(1, language.name, {
-      patientName: appointment.patientName,
-      practitionerAddress: appointment.practitionerAddress,
-      appointmentTime: moment(appointment.appointmentTime).format("LLLL"),
-    });
-
-    const message = {
-      appointmentId,
-      messageBody,
-      language: language.name,
-      templateName: template.templateName,
-      // UTC, will be the same for every message sent in a batch
-      timeSent: moment().format("YYYY-MM-DD HH:mm:ss"),
-    };
-
-    try {
-      await sendMessage(patientPhoneNumber, messageBody);
-    } catch (error) {
-      return {
-        error,
-        message: `Failed to send appointment reminder to ${patientPhoneNumber}`,
-      };
-    }
-
-    let messageId;
-    try {
-      messageId = await db("messages").insert(message);
-    } catch (error) {
-      return {
-        error,
-        message: "Could not save message to database",
-      };
-    }
-    return {
-      appointmentId,
-      messageId: messageId[0],
-    };
+  const language = await LanguagesModel.getByLanguageString(
+    appointment.language
+  );
+  const template = TemplatesModel.getById(1);
+  const messageBody = TemplatesModel.generateMessage(1, language.name, {
+    ...appointment,
+    appointmentDateTime: moment(appointment.appointmentTime).format(
+      "YYYY-MM-DD h:mm a"
+    ),
+    appointmentDate: moment(appointment.appointmentTime).format("YYYY-MM-DD"),
+    appointmentTime: moment(appointment.appointmentTime).format("h:mm a"),
   });
 
-  return Promise.all(appointmentsPromises)
-    .then((result) => res.status(200).send(result))
-    .catch((error) =>
-      res.status(500).send({
-        error,
-        message: `Could not send reminders for ${daysFromNow(1)}`,
-      })
-    );
+  const timeSent = new Date();
+
+  const message = {
+    appointmentId: appointment.id,
+    messageBody,
+    language: language.name,
+    templateName: template.templateName,
+    timeSent,
+  };
+
+  await sendMessage(appointment.patientPhoneNumber, messageBody);
+  await db("messages").insert(message);
+  await db("appointments")
+    .update("lastReminderSentAt", timeSent)
+    .where("appointmentId", appointment.id);
+};
+
+export const sendReminders = async () => {
+  // Select appointments coming up in the next 24 hours that
+  // have not had a notification in the previous 24 hours.
+  const appointments = await db("appointments")
+    .select(
+      "appointmentId as id",
+      "language",
+      "appointmentTime",
+      "patientPhoneNumber",
+      "practitionerAddress",
+      "specialNotes",
+      "description"
+    )
+    .where("isDeleted", false)
+    .where("lastReminderSentAt", "<=", daysFromNow(-1))
+    .orWhereNull("lastReminderSentAt")
+    .where("appointmentTime", ">=", daysFromNow(0))
+    .where("appointmentTime", "<", daysFromNow(1));
+
+  const promises = appointments.map(sendReminder);
+
+  return await Promise.all(promises);
+};
+
+export default async (req, res) => {
+  try {
+    const results = await sendReminders();
+    res.status(200).send(results);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      e,
+      message: `Could not send reminders for ${daysFromNow(1)}`,
+    });
+  }
 };
