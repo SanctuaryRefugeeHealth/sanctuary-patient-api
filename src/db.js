8a030626fffef6404667b09865c21129ea0f684f
Sam Lewis <sam@wiles.co>
2020-10-24 16:11:29 +0100

    Removed unused DB connection

--- src/db.js
+++
@@ -1,7 +0,0 @@
-export default callback => {
-    let knex = require("knex");
-    const knexConfig = require("../knexfile");
-    knex = knex(knexConfig);
-    // connect to a database if needed, then pass it to `callback`:
-    callback(knex);
-};
