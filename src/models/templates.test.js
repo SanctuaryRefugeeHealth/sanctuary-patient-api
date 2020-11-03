6f0e4d53063f10949b845e946ae23067a0808f7b
Kris Braun <kris.braun@gmail.com>
2020-11-02 22:27:08 -0500

    Clean up message formatting

--- src/models/templates.test.js
+++ src/models/templates.test.js
@@ -13,17 +13,14 @@ describe("#generateMessage()", function () {
   it("should return formatted message in English", function () {
     const actual = TemplatesModel.generateMessage(1, "English", commonMetadata);
 
-    const expected = `
-      Dear John Smith, this message is to inform you of your upcoming appointment.
-      Date: 2020-01-02
-      Time: 3:04 am
-      Address: 123 Fake St.
+    const expected = `Dear John Smith, this message is to inform you of your upcoming appointment:
+Date: 2020-01-02
+Time: 3:04 am
+Address: 123 Fake St.
 
-      Please confirm your attendance by replying “Yes” or “No”
-      If you need an interpreter, please reply with the word "interpreter"
-      If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael
-      Stephenson) at 226-336-1321
-    `;
+Please confirm your attendance by replying “Yes” or “No”.
+If you need an interpreter, please reply with the word "interpreter".
+If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael Stephenson) at 226-336-1321.`;
 
     expect(actual).to.equal(expected);
   });
@@ -36,17 +33,15 @@ describe("#generateMessage()", function () {
 
     const actual = TemplatesModel.generateMessage(1, "English", metadata);
 
-    const expected = `
-      Dear John Smith, this message is to inform you of your upcoming appointment: X-ray.
-      Date: 2020-01-02
-      Time: 3:04 am
-      Address: 123 Fake St.
+    const expected = `Dear John Smith, this message is to inform you of your upcoming appointment:
+X-ray
+Date: 2020-01-02
+Time: 3:04 am
+Address: 123 Fake St.
 
-      Please confirm your attendance by replying “Yes” or “No”
-      If you need an interpreter, please reply with the word "interpreter"
-      If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael
-      Stephenson) at 226-336-1321
-    `;
+Please confirm your attendance by replying “Yes” or “No”.
+If you need an interpreter, please reply with the word "interpreter".
+If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael Stephenson) at 226-336-1321.`;
 
     expect(actual).to.equal(expected);
   });
@@ -59,18 +54,15 @@ describe("#generateMessage()", function () {
 
     const actual = TemplatesModel.generateMessage(1, "English", metadata);
 
-    const expected = `
-      Dear John Smith, this message is to inform you of your upcoming appointment.
-      Date: 2020-01-02
-      Time: 3:04 am
-      Address: 123 Fake St.
-      Special Notes: This is a special note
+    const expected = `Dear John Smith, this message is to inform you of your upcoming appointment:
+Date: 2020-01-02
+Time: 3:04 am
+Address: 123 Fake St.
+Special Notes: This is a special note
 
-      Please confirm your attendance by replying “Yes” or “No”
-      If you need an interpreter, please reply with the word "interpreter"
-      If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael
-      Stephenson) at 226-336-1321
-    `;
+Please confirm your attendance by replying “Yes” or “No”.
+If you need an interpreter, please reply with the word "interpreter".
+If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael Stephenson) at 226-336-1321.`;
 
     expect(actual).to.equal(expected);
   });
@@ -84,18 +76,16 @@ describe("#generateMessage()", function () {
 
     const actual = TemplatesModel.generateMessage(1, "English", metadata);
 
-    const expected = `
-      Dear John Smith, this message is to inform you of your upcoming appointment: X-ray.
-      Date: 2020-01-02
-      Time: 3:04 am
-      Address: 123 Fake St.
-      Special Notes: This is a special note
+    const expected = `Dear John Smith, this message is to inform you of your upcoming appointment:
+X-ray
+Date: 2020-01-02
+Time: 3:04 am
+Address: 123 Fake St.
+Special Notes: This is a special note
 
-      Please confirm your attendance by replying “Yes” or “No”
-      If you need an interpreter, please reply with the word "interpreter"
-      If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael
-      Stephenson) at 226-336-1321
-    `;
+Please confirm your attendance by replying “Yes” or “No”.
+If you need an interpreter, please reply with the word "interpreter".
+If you have any questions, please call Sanctuary Refugee Health Centre (Dr. Michael Stephenson) at 226-336-1321.`;
 
     expect(actual).to.equal(expected);
   });
@@ -103,16 +93,14 @@ describe("#generateMessage()", function () {
   it("should return formatted message in Arabic", function () {
     const actual = TemplatesModel.generateMessage(1, "Arabic", commonMetadata);
 
-    const expected = `
-      السّيد(ة) \u200EJohn Smith، هذه رسالة لإعلامك بالموعد
-      التّاريخ: \u200E2020-01-02.
-      الوقت: \u200E3:04 am.
-      العنوان: \u200E123 Fake St..
+    const expected = `السّيد(ة) \u200EJohn Smith، هذه رسالة لإعلامك بالموعد:
+التّاريخ: \u200E2020-01-02
+الوقت: \u200E3:04 am
+العنوان: \u200E123 Fake St.
 
-      يرجى تأكيد الحضور بالإجابة بـِ "نعم" أو "لا".
-      إذا كنتم تحتاجون لمترجم، يرجى الرّد بكلمة "مترجم".
-      إذا كانت لديكم أيّة استفسارات، يرجى الاتّصال بعيادة السانكتشوري (الدكتور مايكل ستيفنسن) على الرّقم التّالي: \u200E226-336-1321
-    `;
+يرجى تأكيد الحضور بالإجابة بـِ "نعم" أو "لا".
+إذا كنتم تحتاجون لمترجم، يرجى الرّد بكلمة "مترجم".
+إذا كانت لديكم أيّة استفسارات، يرجى الاتّصال بعيادة السانكتشوري (الدكتور مايكل ستيفنسن) على الرّقم التّالي: \u200E226-336-1321`;
 
     expect(actual).to.equal(expected);
   });
@@ -120,17 +108,15 @@ describe("#generateMessage()", function () {
   it("should return formatted message in Amharic", function () {
     const actual = TemplatesModel.generateMessage(1, "Amharic", commonMetadata);
 
-    const expected = `
-      ለ John Smith፣ ይህ መልእክት የሚከተለው ቀጠሮ እንዳለዎት ለማሳወቅ ነው፥.
-      ቀን፦ 2020-01-02
-      ሰዐት፦ 3:04 am
-      አድራሻ፦ 123 Fake St.
+    const expected = `ለ John Smith፣ ይህ መልእክት የሚከተለው ቀጠሮ እንዳለዎት ለማሳወቅ ነው፥:
+ቀን፦ 2020-01-02
+ሰዐት፦ 3:04 am
+አድራሻ፦ 123 Fake St.
 
-      በዚህ ቀጠሮ ላይ እገኛለሁ ወይም አልገኝም በማለት እንዲያሳውቁን በትህትና እንጠይቃለን።
-      አስተርጓሚ ካስፈለግዎ፣ እባኮ "አስተርጓሚ" በማለት ይመልሱ።
-      ጥያቄ ካለዎት፣እባኮ በ 226-336-1321 Sanctuary Refugee Health Centre በመደወል (ዶ/ር ማይክል
-      ስቴፈንሰን) ያነጋግሩ።
-    `;
+በዚህ ቀጠሮ ላይ እገኛለሁ ወይም አልገኝም በማለት እንዲያሳውቁን በትህትና እንጠይቃለን።
+አስተርጓሚ ካስፈለግዎ፣ እባኮ "አስተርጓሚ" በማለት ይመልሱ።
+ጥያቄ ካለዎት፣እባኮ በ 226-336-1321 Sanctuary Refugee Health Centre በመደወል (ዶ/ር ማይክል
+ስቴፈንሰን) ያነጋግሩ። `;
 
     expect(actual).to.equal(expected);
   });
@@ -138,17 +124,14 @@ describe("#generateMessage()", function () {
   it("should return formatted message in Somali", function () {
     const actual = TemplatesModel.generateMessage(1, "Somali", commonMetadata);
 
-    const expected = `
-      Gacaliye John Smith, Farriintan ayaa ah in lagu ogeysiiyo ballantaada soo socota.
-      Taariikh: 2020-01-02
-      Waqtiga: 3:04 am
-      Cinwaanka: 123 Fake St.
+    const expected = `Gacaliye John Smith, Farriintan ayaa ah in lagu ogeysiiyo ballantaada soo socota:
+Taariikh: 2020-01-02
+Waqtiga: 3:04 am
+Cinwaanka: 123 Fake St.
 
-      Fadlan xaqiiji imaanshahaaga adoo ku jawaabaya "Haa" ama "Maya"
-      Haddii aad u baahan tahay turjubaan, fadlan ku jawaab ereyga "turjubaan"
-      Haddii aad wax su'aalo ah qabtid, fadlan wac Xarunta Caafimaadka Qaxootiga ee
-      Sanctuary (Dr. Michael Stephenson) lambarka 226-336-1321
-    `;
+Fadlan xaqiiji imaanshahaaga adoo ku jawaabaya "Haa" ama "Maya"
+Haddii aad u baahan tahay turjubaan, fadlan ku jawaab ereyga "turjubaan"
+Haddii aad wax su'aalo ah qabtid, fadlan wac Xarunta Caafimaadka Qaxootiga ee Sanctuary (Dr. Michael Stephenson) lambarka 226-336-1321`;
 
     expect(actual).to.equal(expected);
   });
@@ -156,18 +139,16 @@ describe("#generateMessage()", function () {
   it("should return formatted message in Turkish", function () {
     const actual = TemplatesModel.generateMessage(1, "Turkish", commonMetadata);
 
-    const expected = `
-      Sayin John Smith, Bu mesaj randevunuz ile ilgili sizi bilgilendirme amacli gonderilmistir.
-      Tarih: 2020-01-02
-      Saat: 3:04 am
-      Adres: 123 Fake St.
+    const expected = `Sayin John Smith, Bu mesaj randevunuz ile ilgili sizi bilgilendirme amacli gonderilmistir:
+Tarih: 2020-01-02
+Saat: 3:04 am
+Adres: 123 Fake St.
 
-      Lutfen randevunuza katilim durumunuzu, bu mesaji “Evet” ya da “Hayir” seklinde
-      yanitlayarak bildiriniz
-      Eger tercumana ihtiyaciniz varsa lutfen bu mesaji "tercuman" yazarak yanitlayiniz.
-      Sorulariniz icin lutfen bizi 226-336-1321 numarali telefondan arayiniz.
-      Sanctuary Refugee Health Centre - Dr Michael Stephenson
-    `;
+Lutfen randevunuza katilim durumunuzu, bu mesaji “Evet” ya da “Hayir” seklinde
+yanitlayarak bildiriniz
+Eger tercumana ihtiyaciniz varsa lutfen bu mesaji "tercuman" yazarak yanitlayiniz.
+Sorulariniz icin lutfen bizi 226-336-1321 numarali telefondan arayiniz.
+Sanctuary Refugee Health Centre - Dr Michael Stephenson`;
 
     expect(actual).to.equal(expected);
   });
@@ -175,17 +156,14 @@ describe("#generateMessage()", function () {
   it("should return formatted message in Spanish", function () {
     const actual = TemplatesModel.generateMessage(1, "Spanish", commonMetadata);
 
-    const expected = `
-      Estimado(a) John Smith, Este mensaje es para informarle que usted tiene una próxima cita.
-      Fecha: 2020-01-02
-      Hora: 3:04 am
-      Direccion: 123 Fake St.
+    const expected = `Estimado(a) John Smith, Este mensaje es para informarle que usted tiene una próxima cita:
+Fecha: 2020-01-02
+Hora: 3:04 am
+Direccion: 123 Fake St.
 
-      Por favor confirme su asistencia respondiendo “Si” o “No”
-      Si usted necesita un intérprete, por favor responda con la palabra “intérprete”
-      Si usted tiene alguna pregunta, por favor llame a Sanctuary Refugee Health Centre (Dr. Michael
-      Stephenson) al 226-336-1321.
-    `;
+Por favor confirme su asistencia respondiendo “Si” o “No”
+Si usted necesita un intérprete, por favor responda con la palabra “intérprete”
+Si usted tiene alguna pregunta, por favor llame a Sanctuary Refugee Health Centre (Dr. Michael Stephenson) al 226-336-1321.`;
 
     expect(actual).to.equal(expected);
   });
