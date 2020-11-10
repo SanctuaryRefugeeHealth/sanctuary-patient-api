72339cccb952e15f713649051e9f31ef9cc8ea19
Sam Lewis <sam@wiles.co>
2020-11-10 18:12:03 +0000

    Reduced copy paste in post appointment tests

--- src/api/handlers/appointments/postAppointments.test.js
+++ src/api/handlers/appointments/postAppointments.test.js
@@ -16,6 +16,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", api({ config }));
 
+const baseAppointment = {
+  date: "2020-11-10 21:30",
+  practitionerAddress: "123 Fake St",
+  patientLanguage: "English",
+  patientName: "John Smith",
+  patientPhoneNumber: "5555555555",
+  description: "This is a description",
+  specialNotes: "This is a special note",
+};
+
 describe("POST /appointments", () => {
   before(() => {
     sinon.stub(appointments, "createAppointment").callsFake(() => {
@@ -30,178 +40,116 @@ describe("POST /appointments", () => {
   it("should succeed on valid appointment", (done) => {
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(baseAppointment)
       .expect(200, done);
   });
 
   it("should succeed without description", (done) => {
-    request(app)
-      .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        specialNotes: "This is a special note",
-      })
-      .expect(200, done);
+    const appointment = { ...baseAppointment };
+    delete appointment.description;
+    request(app).post("/api/appointments").send(appointment).expect(200, done);
   });
 
   it("should fail on empty description", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.description = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "",
-        specialNotes: "This is a special note",
-      })
-      .expect(500,{message: '"description" is not allowed to be empty'}, done);
+      .send(appointment)
+      .expect(
+        500,
+        { message: '"description" is not allowed to be empty' },
+        done
+      );
   });
 
-
   it("should fail on whitespace only description", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.description = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "     ",
-        specialNotes: "This is a special note",
-      })
-      .expect(500,{message: '"description" is not allowed to be empty'}, done);
+      .send(appointment)
+      .expect(
+        500,
+        { message: '"description" is not allowed to be empty' },
+        done
+      );
   });
 
   it("should succeed without special notes", (done) => {
-    request(app)
-      .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-      })
-      .expect(200, done);
+    const appointment = { ...baseAppointment };
+    delete appointment.specialNotes;
+    request(app).post("/api/appointments").send(appointment).expect(200, done);
   });
 
   it("should fail on empty special notes", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.specialNotes = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "",
-      })
-      .expect(500, {message: '"specialNotes" is not allowed to be empty'}, done);
+      .send(appointment)
+      .expect(
+        500,
+        { message: '"specialNotes" is not allowed to be empty' },
+        done
+      );
   });
 
   it("should fail on whitespace only special notes", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.specialNotes = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "     ",
-      })
-      .expect(500, {message: '"specialNotes" is not allowed to be empty'}, done);
+      .send(appointment)
+      .expect(
+        500,
+        { message: '"specialNotes" is not allowed to be empty' },
+        done
+      );
   });
 
   it("should fail on empty date", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.date = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"date" must be a valid date' }, done);
   });
 
   it("should fail on whitespace only date", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.date = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "     ",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"date" must be a valid date' }, done);
   });
 
   it("should fail on bad date", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.date = "2020-13-10 21:30";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-13-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"date" must be a valid date' }, done);
   });
 
   it("should fail on missing date", (done) => {
+    const appointment = { ...baseAppointment };
+    delete appointment.date;
     request(app)
       .post("/api/appointments")
-      .send({
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"date" is required' }, done);
   });
 
   it("should fail on empty address", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.practitionerAddress = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"practitionerAddress" is not allowed to be empty' },
@@ -210,17 +158,11 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on whitespace only address", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.practitionerAddress = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "     ",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"practitionerAddress" is not allowed to be empty' },
@@ -229,31 +171,20 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on missing address", (done) => {
+    const appointment = { ...baseAppointment };
+    delete appointment.practitionerAddress;
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"practitionerAddress" is required' }, done);
   });
 
   it("should fail on empty language", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.patientLanguage = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientLanguage" is not allowed to be empty' },
@@ -262,17 +193,11 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on whitespace only language", (done) => {
+    const appointment = { ...baseAppointment };
+    appointment.patientLanguage = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "     ",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientLanguage" is not allowed to be empty' },
@@ -281,31 +206,20 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on missing language", (done) => {
+    const appointment = { ...baseAppointment };
+    delete appointment.patientLanguage;
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientName: "John Smith",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"patientLanguage" is required' }, done);
   });
 
   it("should fail on empty name", function (done) {
+    const appointment = { ...baseAppointment };
+    appointment.patientName = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientName" is not allowed to be empty' },
@@ -314,17 +228,11 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on whitespace only name", function (done) {
+    const appointment = { ...baseAppointment };
+    appointment.patientName = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "     ",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientName" is not allowed to be empty' },
@@ -333,31 +241,20 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on missing name", function (done) {
+    const appointment = { ...baseAppointment };
+    delete appointment.patientName;
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientPhoneNumber: "5555555555",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"patientName" is required' }, done);
   });
 
   it("should fail on empty phone number", function (done) {
+    const appointment = { ...baseAppointment };
+    appointment.patientPhoneNumber = "";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientPhoneNumber" is not allowed to be empty' },
@@ -366,17 +263,11 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on whitespace only phone number", function (done) {
+    const appointment = { ...baseAppointment };
+    appointment.patientPhoneNumber = "     ";
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        patientPhoneNumber: "     ",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(
         500,
         { message: '"patientPhoneNumber" is not allowed to be empty' },
@@ -385,16 +276,11 @@ describe("POST /appointments", () => {
   });
 
   it("should fail on missing phone number", function (done) {
+    const appointment = { ...baseAppointment };
+    delete appointment.patientPhoneNumber;
     request(app)
       .post("/api/appointments")
-      .send({
-        date: "2020-11-10 21:30",
-        practitionerAddress: "123 Fake St",
-        patientLanguage: "English",
-        patientName: "John Smith",
-        description: "This is a description",
-        specialNotes: "This is a special note",
-      })
+      .send(appointment)
       .expect(500, { message: '"patientPhoneNumber" is required' }, done);
   });
 
