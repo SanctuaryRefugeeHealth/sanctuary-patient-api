b3bc7e924c303d4878ec058429337f0414931759
Tahsin <tahsingenuies@gmail.com>
2021-11-16 14:23:45 +0700

    make application class

--- src/index.js
+++ src/index.js
@@ -9,46 +9,41 @@ import api from "./api";
 import { config } from "./config";
 import reminderScheduler from "./services/scheduler";
 
-let app = express();
-app.server = http.createServer(app);
+class Application {
+  constructor() {
+    this.app = express();
+  }
+  start() {
+    this.app.server = http.createServer(this.app);
+    this.app.use(morgan("dev"));
+    // 3rd party middleware
+    this.app.use(cors({ exposedHeaders: config.corsHeaders }));
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json({ limit: config.bodyLimit }));
 
-// logger
-app.use(morgan("dev"));
+    // logger
 
-// 3rd party middleware
-app.use(
-  cors({
-    exposedHeaders: config.corsHeaders,
-  })
-);
+    reminderScheduler.start();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(
+      jwt({ secret: config.jwtConfig.jwtSecret, algorithms: ["HS256"] }).unless(
+        {
+          path: [
+            "/api/twilio/reply", // Authentication handled by Twilio middleware
+            "/api/ping", // Open health check
+            "/api/auth", // Used to login
+          ],
+        }
+      )
+    );
 
-app.use(
-  bodyParser.json({
-    limit: config.bodyLimit,
-  })
-);
+    this.app.use("/api", api({ config }));
 
-reminderScheduler.start();
+    this.app.server.listen(config.port, () => {
+      console.log(`Started on port ${this.app.server.address().port}`);
+    });
+  }
+}
 
-app.use(
-  jwt({
-    secret: config.jwtConfig.jwtSecret,
-    algorithms: ["HS256"],
-  }).unless({
-    path: [
-      "/api/twilio/reply", // Authentication handled by Twilio middleware
-      "/api/ping", // Open health check
-      "/api/auth", // Used to login
-    ],
-  })
-);
-
-app.use("/api", api({ config }));
-
-app.server.listen(config.port, () => {
-  console.log(`Started on port ${app.server.address().port}`);
-});
-
-export default app;
+const application = new Application();
+application.start();
