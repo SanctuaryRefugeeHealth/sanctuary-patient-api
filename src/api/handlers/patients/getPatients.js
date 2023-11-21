137465ab27357c60559d0d394e2d2123d3e93bf2
Tim Collins <trcollins@gmail.com>
2023-11-20 21:50:08 -0500

    Fix prettier warnings

--- src/api/handlers/patients/getPatients.js
+++ src/api/handlers/patients/getPatients.js
@@ -1,16 +1,13 @@
 import { getPatients } from "../../../models/patient";
 
-export default async(req, res) => {
+export default async (req, res) => {
+  const filter = req.query.filter;
 
-    const filter = req.query.filter;
+  if (filter === null || typeof filter !== "string" || filter.length < 3) {
+    return res.status(400).send({
+      message: "Invalid filter criteria.",
+    });
+  }
 
-    if (filter === null ||
-        typeof filter !== 'string' ||
-        filter.length < 3) {
-            return res.status(400).send({
-                message: "Invalid filter criteria."
-            });
-        }
-
-    return res.status(200).send(await getPatients(filter));
-}
+  return res.status(200).send(await getPatients(filter));
+};
